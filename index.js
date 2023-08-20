22ba3d9dd6599f3214281798578cbe9a60207fc2
Ogaraga <raymond.ogaraga@gmail.com>
2023-08-20 13:16:00 +0100

    review

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
+//import a module called express
 var express = require("express");
 
-var app = express();
+var app = express();//initialize app;
 
-let port = 5500;
+let PORT = process.env.PORT || 5500;//setting up the port server
 
+// SETTING TIME OF OPENING/CLOSING OFFICE
 let day = new Date().getDay(); 
-
 let hr = new Date().getHours(); 
 
+//set up a middleware for timing conditions control
 
 let logger = function (req, res, next) {
     if (!(day < 6 && hr <= 17)) {
@@ -22,7 +24,7 @@ let logger = function (req, res, next) {
 }
 app.use(logger);
 
-app.set("view engine", "ejs");
+app.set("view engine", "ejs");//template engine
 
 app.get("/", (req, res) => {
     res.render("pages/Home")
@@ -38,4 +40,4 @@ app.get("/Services", (req, res) => {
 
 app.use("/public", express.static("public"));
 
-app.listen(port, console.log(`App listening on port ${port} @ ` + new Date().getHours()+"hrs: "+ new Date().getMinutes()+"mins: "+new Date().getSeconds()+"sec"));
+app.listen(PORT, console.log(`App listening on port ${PORT} @ ` + new Date().getHours()+"hrs: "+ new Date().getMinutes()+"mins: "+new Date().getSeconds()+"sec"));//listening to server
